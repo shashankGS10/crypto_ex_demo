48fdf3fb14168664d99dd3f94cce2412dc6f8116
Shashank G S <gsshashank@gmail.com>
2025-03-05 19:43:06 +0530

    build test -- pre-deployment -- iteration 2

--- components/CMC100Card.tsx
+++ components/CMC100Card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { SparkLineChart } from "@mui/x-charts/SparklineChart";
+import { SparkLineChart } from "@mui/x-charts";
 import { fetchCryptoData, CryptoData } from "@/utils/apiService";
 
 const LoadingPlaceholder = () => (
